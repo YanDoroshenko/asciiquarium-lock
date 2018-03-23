909f675e10a1498c08f0eb549cdb6d08def29e54
Yan Doroshenko <yan1994doroshenko@gmail.com>
2018-03-23 16:53:33 +0100

    [upd] Move from xterm to xfce4-terminal for deterministic behavior

--- asciiquarium-lock.sh
+++ asciiquarium-lock.sh
@@ -4,8 +4,8 @@ if [ ! $(command -v asciiquarium) ]; then
     echo "\"command not found: \"asciiquarium\"" >&2
     exit 1
 fi
-if [ ! $(command -v xterm) ]; then
-    echo "\"command not found: \"xterm\"" >&2
+if [ ! $(command -v xfce4-terminal) ]; then
+    echo "\"command not found: \"xfce4-terminal\"" >&2
     exit 1
 fi
 if [ ! $(command -v xtrlock) ]; then
@@ -17,7 +17,7 @@ fi
 if [ $(command -v xrandr) ]; then
     OFFSETS=$(\xrandr | \grep -w connected | \awk '{split($0, line, " "); if (line[3] == "primary") { split(line[4], offsets, "+"); print "+"offsets[2]"+"offsets[3] } else { split(line[3], offsets, "+"); print "+"offsets[2]"+"offsets[3] }}')
     for o in $OFFSETS; do
-        \xterm -geometry "0x0$o" -fullscreen -e asciiquarium &
+        \xfce4-terminal --geometry 0x0$o --fullscreen --hide-menubar --hide-scrollbar --hide-borders -e asciiquarium &
     done
 else
     xterm -fullscreen -e asciiquarium &
